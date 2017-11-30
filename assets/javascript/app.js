bd4f2b96fce20df2533e56cc535eeade54e45d93
Ovie <you@example.com>
2017-11-30 10:40:51 -0500

    adds top stories and puts search stories in console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,30 @@
 $(document).ready(function () {
 
-  // //get api info for top stories
-  // var apiKey = "cf2e5faa5f0b4dec846b014e6e89766b";
-  // var queryUrl = 'http://api.nytimes.com/svc/archive/v1/2016/11.json?api-key="+apiKey+';
-  var search = $('#search-text').val();
-    var searchURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=2d6d8bb60364453d8ab1e776b1a25537&q=" + search;
 
-  // $.ajax({
-  //         url: queryURL,
-  //         method: "GET"
-  //       }).done(function(response) {
-  //       	//get headlines and small summary
-  //       	//get the div for the results box
-  //       	//populate the results box
-   	for (var i = 0; i < 10; i++) {
-      		var headline = docs[i].abstract;
-      		var snippet = docs[i].snippet;
+//get api info for top stories
+var apiKey = "cf2e5faa5f0b4dec846b014e6e89766b";
+var queryUrl = 'http://api.nytimes.com/svc/archive/v1/2016/11.json?api-key='+apiKey;
 
-          //var articleNum = $("<span>").text(i);
-          //console.log(articleNum);
-      		//put headline and snippet into results div
-      		$(".news-headline").append("<p>"+"<span>"+i+"</span>"+snippet+"</p>");
+$.ajax({
+        url: queryUrl,
+        method: "GET"
+      }).done(function(article) {
+        //get headlines and small summary
+        console.log(article);
+        var docs = article.response.docs;
 
-      	}
+        for (var i = 0; i < 10; i++) {
+          var headline = docs[i].abstract;
+          var snippet = docs[i].snippet;
+          //put headline and snippet into results div
+          $(".news-headline").append("<p>"+"<span>"+i+"</span>"+snippet+"</p>");
+        }
 
-  //       });
+        
+        //get the div for the results box
+        //populate the results box
 
+      });
 
 
   //get api info for search term coming from input boxes
@@ -72,4 +71,4 @@ $(document).ready(function () {
 
 
 
-});
+})
